0c86070e2e909c6295b97e99d19ef77b6336d86d
Michael Ballard <ballard.michael9@gmail.com>
2018-05-08 02:58:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@ katz_deli = [ ]
 
 def line(array)
   line_count = 0
-  line = "The line is currently: "
+  line = ["The line is currently: "]
   
   if array.length == 0 
     puts "The line is currently empty."
   else
     while line_count < array.length
-      line.( "#{line_count+1 }. ") 
-      line.concat( " #{array[line_count]}")
+      line.join( ["#{line_count+1 }."]) 
+      line.join( [" #{array[line_count]}"])
       line_count +=1
     end
     puts line
