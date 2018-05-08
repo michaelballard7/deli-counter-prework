7747f1ecb3c01cbfcd102dc05701aed6b97abd68
Michael Ballard <ballard.michael9@gmail.com>
2018-05-08 02:45:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
     puts "The line is currently empty."
   else
     while line_count < array.length
-      puts "#{line_count +1}. #{array[line_count]} "
+      puts "#{line_count}. #{array[line_count]} "
       line_count +=1
     end
   end
