73ff0ccff790398f7bd6cf7130053a83a3ea200f
Michael Ballard <ballard.michael9@gmail.com>
2018-05-08 02:42:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
 katz_deli = [ ]
 
 def line(array)
+  line_count = 0
   
   if array.length == 0 
     puts "The line is currently empty."
   else
-     array.each do |i|
-      puts "#{i}. #{array[i]}"
+    while line_count < array.length
+      
+     
     end
   end
 end 
